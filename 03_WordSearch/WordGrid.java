16ddafc9367458be2d67a90e9cef57916598bf4c
Jion Fairchild <jijiglobe@yahoo.com>
2014-11-20 13:04:20 -0500

    updated to WordGrid stable V1

--- 03_WordSearch/WordGrid.java
+++ 03_WordSearch/WordGrid.java
@@ -3,30 +3,39 @@ public class WordGrid{
     
     public void clear(){
 	for(int i=0;i<grid.length;i++){
-	    for(int t;t<grid[i].length;t++){
+	    for(int t=0;t<grid[i].length;t++){
 		grid[i][t] = ' ';
 	    }
 	}
     }
 
-    public string toString(){
-
+    public String toString(){
+	String ans="";
 	for(int i=0;i<grid.length;i++){
-	    String thisline;	    
-	    for(int t;t<grid[i].length;t++){
-		thisline+=grid[i][t]
+	    String thisline = "";	    
+	    for(int t=0;t<grid[i].length;t++){
+		thisline+=grid[i][t];
 	    }
-	    return thisline;
+	    ans+=thisline+"\n";
 	}
+	return ans;
     }	
 
     public WordGrid(int width, int height){
 	grid = new char[height][width];
-	grid.clear();
+	clear();
     }
     
+    public void addWordHorizontal(int x, int y, String word){
+	for(int i=0; i<word.length();i++){
+	    grid[y][x+i] = word.charAt(i);
+	}
+    }
     public static void main(String[]args){
-	wordgrid = new WordGrid(3,3);
+	WordGrid wordgrid = new WordGrid(3,3);
+	wordgrid.addWordHorizontal(0,1,"hi");
+	wordgrid.addWordHorizontal(1,0,"hi");
+
 	System.out.println(wordgrid);
     }
 }
